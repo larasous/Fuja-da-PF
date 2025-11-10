f4f7032fc7e532565c4bb104b81bf895853894cf
larasous <sousalara822@gmail.com>
2025-11-10 17:45:56 -0300

    :sparkles: feat: Definindo proporção fixa e incluind flag para tela maximizada

--- src/engine/window.py
+++ src/engine/window.py
@@ -1,8 +1,6 @@
 import glfw
 from OpenGL.GL import *
-from src.constants.metrics import WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE
-from src.constants.colors import COLOR_PALETTE
-from src.constants.shaders_path import VERTEX_BASIC, FRAGMENT_BASIC
+from src.constants import metrics, colors, shaders_path
 from src.utils.colors import lerp_color
 from src.engine.shader import Shader
 import numpy as np
@@ -11,24 +9,34 @@ from ctypes import c_void_p
 
 class Window:
     def __init__(self):
+
         if not glfw.init():
-            raise Exception("GLFW could not be initialized")
+            raise Exception("❌ GLFW could not be initialized")
+
+        if metrics.WINDOW_MAXIMIZED:
+            glfw.window_hint(glfw.MAXIMIZED, glfw.TRUE)
 
         self.window = glfw.create_window(
-            WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE, None, None
+            metrics.WINDOW_WIDTH,
+            metrics.WINDOW_HEIGHT,
+            metrics.WINDOW_TITLE,
+            None,
+            None,
         )
         if not self.window:
             glfw.terminate()
             raise Exception("Failed to create GLFW window")
 
         glfw.make_context_current(self.window)
-        glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT)
 
-        # Carrega os shaders a partir dos arquivos
-        with open(VERTEX_BASIC, "r") as f:
-            vertex_source = f.read()
-        with open(FRAGMENT_BASIC, "r") as f:
-            fragment_source = f.read()
+        glfw.set_window_size_callback(self.window, self._on_resize)
+
+        self._update_metrics()
+
+        with open(shaders_path.VERTEX_BASIC, "r") as file:
+            vertex_source = file.read()
+        with open(shaders_path.FRAGMENT_BASIC, "r") as file:
+            fragment_source = file.read()
 
         self.shader = Shader(vertex_source, fragment_source)
         self.vao = self._create_triangle()
@@ -37,16 +45,16 @@ class Window:
         while not glfw.window_should_close(self.window):
             glfw.poll_events()
 
-            t = glfw.get_time()
+            # Efeito de transição de cor
+            time = glfw.get_time()
             speed = 2.0
+            index = int(time // speed) % len(colors.COLOR_PALETTE)
+            next_index = (index + 1) % len(colors.COLOR_PALETTE)
+            blend = (time % speed) / speed
 
-            index = int(t // speed) % len(COLOR_PALETTE)
-            next_index = (index + 1) % len(COLOR_PALETTE)
-            blend = (t % speed) / speed
-
-            c1 = COLOR_PALETTE[index]
-            c2 = COLOR_PALETTE[next_index]
-            current_color = lerp_color(c1, c2, blend)
+            color_one = colors.COLOR_PALETTE[index]
+            color_two = colors.COLOR_PALETTE[next_index]
+            current_color = lerp_color(color_one, color_two, blend)
 
             glClearColor(*current_color)
             glClear(GL_COLOR_BUFFER_BIT)
@@ -60,8 +68,10 @@ class Window:
         glfw.terminate()
 
     def _create_triangle(self):
+        """Cria um triângulo simples para renderização."""
         vertices = np.array(
-            [-0.5, -0.5, 0.0, 0.5, -0.5, 0.0, 0.0, 0.5, 0.0], dtype=np.float32
+            [-0.5, -0.5, 0.0, 0.5, -0.5, 0.0, 0.0, 0.5, 0.0],
+            dtype=np.float32,
         )
 
         vao = glGenVertexArrays(1)
@@ -78,3 +88,16 @@ class Window:
         glBindVertexArray(0)
 
         return vao
+
+    def _update_metrics(self):
+        """Atualiza as métricas com base no framebuffer atual."""
+        width, height = glfw.get_framebuffer_size(self.window)
+        metrics.WINDOW_WIDTH = width
+        metrics.WINDOW_HEIGHT = height
+        glViewport(0, 0, width, height)
+
+    def _on_resize(self, window, width, height):
+        """Callback de redimensionamento da janela."""
+        metrics.WINDOW_WIDTH = width
+        metrics.WINDOW_HEIGHT = height
+        glViewport(0, 0, width, height)
