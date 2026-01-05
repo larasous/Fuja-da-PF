60067787a9fd1afe5340cd5fffd2a36ab2b517b7
larasous <sousalara822@gmail.com>
2026-01-05 14:10:53 -0300

    :sparkles: feat: Adição de estrutura para objetos e obstáculos com suporte à transformação e shader

--- src/objects/objects.py
+++ src/objects/objects.py
@@ -1,5 +1,6 @@
 import numpy as np
 from OpenGL.GL import *
+from pyrr import Matrix44
 from src.objects.model import Model
 
 
@@ -11,19 +12,18 @@ class Object:
         self.scale = np.array(scale, dtype=np.float32)
 
     def get_model_matrix(self):
-        # retorna a matriz de transformação TRS
-        pass
-
-    def update(self):
-        """Sobrescrito por Player ou Obstacles"""
-        pass
+        translation = Matrix44.from_translation(self.position)
+        rotation = Matrix44.identity()
+        scaling = Matrix44.from_scale(self.scale)
+        model_matrix = translation @ rotation @ scaling
+        return model_matrix
 
     def render(self, shader):
         shader.set_mat4("model", self.get_model_matrix())
         self.model.draw()
 
 
-class Obstacle:
+class Obstacle(Object):
     def __init__(
         self,
         model: Model,
@@ -32,19 +32,18 @@ class Obstacle:
         scale: float = 0.5,
         color=(1.0, 0.0, 0.0),
     ):
-        self.model = model
-        self.x = x
-        self.z = z
-        self.scale = scale
-        self.color = color
+        super().__init__(
+            model=model,
+            position=[x, 0.0, z],
+            rotation=[0.0, 0.0, 0.0],
+            scale=[scale, scale, scale],
+        )
+        self.color = np.array(color, dtype=np.float32)
 
     def update(self, speed: float):
-        self.z += speed
+        self.position[2] += speed
 
-    def draw(self):
-        glPushMatrix()
-        glTranslatef(self.x, 0.0, self.z)
-        glScalef(self.scale, self.scale, self.scale)
-        glColor3f(*self.color)
+    def render(self, shader):
+        shader.set_mat4("model", self.get_model_matrix())
+        shader.set_vec3("color", self.color)
         self.model.draw()
-        glPopMatrix()
