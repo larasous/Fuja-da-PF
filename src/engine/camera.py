a69c8afb5d365f75e18a5202b6588caf3d7f5e38
Marcelo Albuquerque <marcelo03@alu.ufc.br>
2026-01-05 11:46:06 -0300

    feat: criando as tres cameras

--- src/engine/camera.py
+++ src/engine/camera.py
@@ -1,3 +1,46 @@
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
 class CameraManager:
     def __init__(self):
-        pass
+        self.mode = "third_person"
+
+        self.current_pos = [0.0, 5.0, 7.0]
+        self.current_target = [0.0, 0.0, -10.0]
+
+        self.target_pos = self.current_pos[:]
+        self.target_target = self.current_target[:]
+
+        self.transition_speed = 0.01
+
+    def set_mode(self, mode: str):
+        if mode in ("first_person", "third_person", "top_down"):
+            self.mode = mode
+
+    def apply(self, player_x, player_y, player_z):
+        glMatrixMode(GL_MODELVIEW)
+        glLoadIdentity()
+
+        if self.mode == "first_person":
+            self.target_pos = [player_x, player_y + 0.5, player_z]
+            self.target_target = [player_x, player_y + 0.5, player_z - 5.0]
+
+        elif self.mode == "third_person":
+            self.target_pos = [0.0, 5.0, 7.0]
+            self.target_target = [0.0, 0.0, -10.0]
+
+        elif self.mode == "top_down":
+            self.target_pos = [player_x, 20.0, 0.0]
+            self.target_target = [player_x, 0.0, player_z - 5.0]
+        
+        # --- interpolação suave ---
+        for i in range(3):
+            self.current_pos[i] += (self.target_pos[i] - self.current_pos[i]) * self.transition_speed
+            self.current_target[i] += (self.target_target[i] - self.current_target[i]) * self.transition_speed
+
+        gluLookAt(
+            *self.current_pos,
+            *self.current_target,
+            0.0, 1.0, 0.0
+        )
+
--- src/objects/player.py
+++ src/objects/player.py
@@ -3,26 +3,41 @@ from OpenGL.GL import *
 from OpenGL.GLU import *
 
 class Player:
-    def __init__(self, lanes, start_lane=1):
+    def __init__(self, lanes):
         self.lanes = lanes
-        self.current_lane = start_lane
+        self.current_lane = 1
 
-        # atributos visuais
-        self.color = (0.0, 1.0, 0.0)  # verde
-        self.radius = 0.1             # tamanho da esfera
-        self.z = -3.0  # posição fixa em z
+        self.target_lane = 1
+        self.x = lanes[self.current_lane]
+        self.z = -3.0 
+        self.speed = 0.05
+
+        self.color = (0.0, 1.0, 0.0)  
+        self.radius = 0.1             
+        
 
     def move_left(self):
-        if self.current_lane > 0:
-            self.current_lane -= 1
+        if self.target_lane > 0:
+            self.target_lane -= 1
 
     def move_right(self):
-        if self.current_lane < len(self.lanes) - 1:
-            self.current_lane += 1
+        if self.target_lane < len(self.lanes) - 1:
+            self.target_lane += 1
+
+    def update(self):
+        target_x = self.lanes[self.target_lane]
+
+        if abs(self.x - target_x) > 0.01:
+            direction = 1 if target_x > self.x else -1
+            self.x += direction * self.speed
+        else:
+            self.x = target_x
+            self.current_lane = self.target_lane
+
 
     def draw(self):
         glPushMatrix()
-        glTranslatef(self.lanes[self.current_lane], 0.0, -3.0)  # z ajustado
+        glTranslatef(self.x, 0.0, self.z)  
         glColor3f(*self.color)
 
         quadric = gluNewQuadric()
