c631be641383df6398a197657981acad54f1bdfb
larasous <sousalara822@gmail.com>
2026-01-05 13:48:35 -0300

    :sparkles: feat: Adição de constantes de shaders e texturas

--- src/constants/shaders_path.py
+++ src/constants/shaders_path.py
@@ -1,2 +1,4 @@
-VERTEX_BASIC = "assets/shaders/simple.vert"
-FRAGMENT_BASIC = "assets/shaders/simple.frag"
+VERTEX_SKYBOX = "assets/shaders/skybox.vert"
+FRAGMENT_SKYBOX = "assets/shaders/skybox.frag"
+VERTEX_FRENCH_FRIES = "assets/shaders/french_fries.vert"
+FRAGMENT_FRENCH_FRIES = "assets/shaders/french_fries.frag"
---
+++ src/constants/textures_path.py
@@ -0,0 +1,9 @@
+# Skybox textures (panorama convertido para cubemap)
+SKYBOX_TEXTURES = {
+    "PX": "assets/textures/skybox/px.png",  # Positive X (direita)
+    "NX": "assets/textures/skybox/nx.png",  # Negative X (esquerda)
+    "PY": "assets/textures/skybox/py.png",  # Positive Y (cima)
+    "NY": "assets/textures/skybox/ny.png",  # Negative Y (baixo)
+    "PZ": "assets/textures/skybox/pz.png",  # Positive Z (frente)
+    "NZ": "assets/textures/skybox/nz.png",  # Negative Z (trás)
+}
