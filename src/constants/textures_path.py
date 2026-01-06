b63d1e34bc1c7cf0397b51f6f9c7d34405a718cc
larasous <sousalara822@gmail.com>
2026-01-06 19:17:38 -0300

    :sparkles: feat: Adição de texturas para pista

--- src/constants/textures_path.py
+++ src/constants/textures_path.py
@@ -7,3 +7,12 @@ SKYBOX_TEXTURES = {
     "PZ": "assets/textures/skybox/pz.png",  # Positive Z (frente)
     "NZ": "assets/textures/skybox/nz.png",  # Negative Z (trás)
 }
+
+LANES_TEXTURES = {
+    "PX": "assets/textures/lanes/px.png",  # Positive X (direita)
+    "NX": "assets/textures/lanes/nx.png",  # Negative X (esquerda)
+    "PY": "assets/textures/lanes/py.png",  # Positive Y (cima)
+    "NY": "assets/textures/lanes/ny.png",  # Negative Y (baixo)
+    "PZ": "assets/textures/lanes/pz.png",  # Positive Z (frente)
+    "NZ": "assets/textures/lanes/nz.png",  # Negative Z (trás)
+}
